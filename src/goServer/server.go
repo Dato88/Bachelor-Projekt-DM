46271ef794c237882aead17e722b4acf51099e9d
Dato88 <andrej_miller@outlook.de>
2021-01-25 17:25:41 +0100

    klammer

--- src/goServer/server.go
+++ src/goServer/server.go
@@ -193,7 +193,7 @@ func allAcc(w http.ResponseWriter) {
 func insertMSG(fdNummer string, GroupID string, message string) {
 
 	fmt.Println("insertMSG wurde aufgerufen")
-	stmt, err := mainDB.Prepare("INSERT INTO nachrichten(fdNummer, GroupID, message) values (?, ?, ?")
+	stmt, err := mainDB.Prepare("INSERT INTO nachrichten(fdNummer, GroupID, message) values (?, ?, ?)")
 	checkErr(err)
 
 	result, errExec := stmt.Exec(fdNummer, GroupID, message)
