a248eea28b17717190ee7ca6aa7d4676769958c7
Dato88 <andrej_miller@outlook.de>
2021-03-09 12:10:30 +0100

    4

--- src/goServer/server.go
+++ src/goServer/server.go
@@ -217,7 +217,7 @@ func insertMSG(fdNummer string, GroupID string, message string) {
 	fmt.Println("insertMSG wurde aufgerufen")
 	stmt, err := mainDB.Prepare("INSERT INTO nachrichten(fdNummer, GroupID, message) values (?, ?, ?)")
 	checkErr(err)
-
+	fmt.Println("Message wurde gespeichert!!")
 	result, errExec := stmt.Exec(fdNummer, GroupID, message)
 	checkErr(errExec)
 
