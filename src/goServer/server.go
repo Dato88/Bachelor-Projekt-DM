633a15e597e6672582770cca6f96b5e4203ec181
Dato88 <andrej_miller@outlook.de>
2021-03-16 13:21:04 +0100

    l

--- src/goServer/server.go
+++ src/goServer/server.go
@@ -307,8 +307,7 @@ func groupRows(w http.ResponseWriter, rows *sql.Rows) {
 		// 	string(vorname), string(groupName), string(message), string(gesUhrzeit))
 	}
 
-	// parsedTemplate, _ := template.ParseFiles("templates/chat1.html")
-	parsedTemplate, _ := template.ParseFiles("http://bachelor-community.informatik.hs-fulda.de/templates/chat1.html")
+	parsedTemplate, _ := template.ParseFiles("templates/chat1.html")
 	err := parsedTemplate.Execute(w, chat)
 	if err != nil {
 		log.Println("Fehler beim Ausführen der Template: ", err)
